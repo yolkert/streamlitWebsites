f7c190ff09c8ecea6b4a31eebf6b7e2393382f40
yolkert <ethanhumrich@gmail.com>
2024-01-27 21:59:03 -0800

    Website :smile:

---
+++ Homepage.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="Ethans Homepage", page_icon=":house_with_garden:", layout="wide")
+
+st.sidebar.success("Select a page above.")
+
+st.subheader("Hi, I am Ethan. Welcome to my homepage.")
+st.title("A nerd.")
+st.image('swimming.png', caption='*This is a doctored image*')
+
+st.subheader("Venture forth into my website to learn a little more about me.")
---
+++ pages/About_Me.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(page_title="Ethans Homepage", page_icon=":house_with_garden:", layout="wide")
+
+st.sidebar.success("Select a page above.")
+
+st.title("About Ethan...")
+
+st.image('MotorBoat.jpg', caption='Me at lake Chelan in 2021', width=700)
+st.subheader("I was born in 2001 in Arlington Washington and I have never lived anywhere else.")
+st.subheader("I worked at a bakery before going to college when I was 20.")
+st.subheader("My love for programming started because of my love for videogames.")
+st.subheader("My hobbies include but are only sort of limited to: cooking, videogames, music, football, playing with my dogs.")
+st.subheader("")
+
+st.write("[Ethan's Outdated Portfolio](https://ethanhumrich3.wixsite.com/portfolio)")
